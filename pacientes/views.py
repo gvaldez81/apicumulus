f51583369eabe1303d617c4a27ddff8def4b2a9b
lorenalama <llama@ecaresoft.com.mx>
2015-10-10 01:16:55 -0500

    permisos de hosp. recetas

--- pacientes/views.py
+++ pacientes/views.py
@@ -30,6 +30,61 @@ def paciente(request, paciente_id):
         return JsonResponse({'error': 'No permitido'})
 
 
+@csrf_exempt
+def paciente(request, paciente_id, hospital_id):
+    try:
+        paciente = Paciente.objects.get(paciente=paciente_id)
+    except Paciente.DoesNotExist:
+        return JsonResponse({'error': 'Ese paciente no existe'})
+
+    try:
+        hospital = Hospital.objects.get(paciente=paciente_id)
+    except Hospital.DoesNotExist:
+        return JsonResponse({'error': 'Ese hospital no existe'})
+
+    if request.method == 'POST':
+        paciente.hospitales.add(hospital)
+
+        return JsonResponse({'mensaje': 'Se dio permiso al hospital'})
+
+    elif request.method == 'DELETE':
+        paciente.hospitales.remove(hospital)
+    
+        return JsonResponse({'mensaje': 'Se removio el permiso al hospital'})
+
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
+
+def paciente_por_curp(request, curp):
+    if request.method == 'GET':
+        try:
+            paciente = Paciente.objects.get(curp=curp)
+        except Paciente.DoesNotExist:
+            return JsonResponse({'error': 'Ese paciente no existe'})
+
+        return JsonResponse({'id': paciente.id})
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
+# obtiene el paciente solo si el hospital tiene permisos
+def paciente_por_hospital(request, curp, hospital_id):
+    if request.method == 'GET':
+        try:
+            paciente = Paciente.objects.get(curp=curp)
+        except Paciente.DoesNotExist:
+            return JsonResponse({'error': 'Ese paciente no existe'})
+
+        try:
+            hospital = paciente.hospitales.get(id=hospital_id)
+        except Hospital.DoesNotExist:
+            return JsonResponse({'error': 'Ese hospital no tiene permisos'})
+
+        return JsonResponse({'id': paciente.id})
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
+
 def eventos(request, paciente_id):
     if request.method == 'GET':
         eventos = Evento.objects.filter(paciente=paciente_id)
@@ -65,6 +120,19 @@ def eventos(request, paciente_id):
             evento_json['intervenciones'] = inter_json
 
         return JsonResponse(eventos_json, safe=False)
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
+
+def historia(request, paciente_id):
+    if request.method == 'GET':
+        try:
+            historia = Historia.objects.get(paciente=paciente_id)
+        except Historia.DoesNotExist:
+            return JsonResponse({'error': 'Ese paciente no existe'})
+
+        hist_json = to_json(historia)
+        return JsonResponse(hist_json, safe=False)
     else:
         return JsonResponse({'error': 'No permitido'})
 
@@ -163,3 +231,23 @@ def tomas_signos(request, paciente_id):
         return JsonResponse(tomas_json, safe=False)
     else:
         return JsonResponse({'error': 'No permitido'})
+
+
+def recetas(request, paciente_id):
+    if request.method == 'GET':
+        recetas = Receta.objects.filter(paciente=paciente_id)
+
+        if not recetas:
+            return JsonResponse({'warning': 'Paciente sin recetas'})
+
+        recetas_json = to_json(recetas, True)
+
+        for receta_json in recetas_json:
+	    meds = Medicamento.objects.filter(receta=receta_json['id'])
+            meds_json = to_json(meds, True)
+            receta_json['medicamentos'] = meds_json
+
+        return JsonResponse(recetas_json, safe=False)
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
