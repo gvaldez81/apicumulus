205e03aedf131a0fcd9b709818d5f92798ed95d2
lorenalama <llama@ecaresoft.com.mx>
2015-10-09 22:54:59 -0500

    consultas

--- pacientes/views.py
+++ pacientes/views.py
@@ -27,6 +27,7 @@ def paciente(request, paciente_id):
     else:
         return JsonResponse({'error': 'No permitido'})
 
+
 def eventos(request, paciente_id):
     if request.method == 'GET':
         eventos = Evento.objects.filter(paciente=paciente_id)
@@ -34,11 +35,38 @@ def eventos(request, paciente_id):
         if not eventos:
             return JsonResponse({'warning': 'Paciente sin eventos'})
 
-        eve_json = to_json(eventos, True)
+        eventos_json = to_json(eventos, True)
+
+        for evento_json in eventos_json:
+	    
+            tomas_json = to_json(Tomas.objects.filter(evento=evento_json['id']), True)
+
+            for toma_json in tomas_json:
+                signos = SignoVital.objects.filter(toma=toma_json['id'])
+                signos_json = to_json(signos, True)
+	        toma_json['signos'] = signos_json
+
+            evento_json['tomas'] = tomas_json
+
+	    recetas_json = to_json(Receta.objects.filter(evento=evento_json['id']), True)
+
+            for receta_json in recetas_json:
+                meds = Medicamento.objects.filter(receta=receta_json['id'])
+                meds_json = to_json(meds, True)
+	        receta_json['medicamentos'] = meds_json
+
+	    evento_json['recetas'] = recetas_json
+
+	    diags_json = to_json(Diagnostico.objects.filter(evento=evento_json['id']), True)
+	    evento_json['diagnosticos'] = diags_json
+
+	    inter_json = to_json(Intervencion.objects.filter(evento=evento_json['id']), True)
+	    evento_json['intervenciones'] = inter_json
 
         return JsonResponse(eve_json, safe=False)
     else:
         return JsonResponse({'error': 'No permitido'})
+
 
 def alergias(request, paciente_id):
     if request.method == 'GET':
@@ -52,6 +80,7 @@ def alergias(request, paciente_id):
     else:
         return JsonResponse({'error': 'No permitido'})
 
+
 def diagnosticos(request, paciente_id):
     if request.method == 'GET':
         diags = Diagnostico.objects.filter(paciente=paciente_id)
@@ -64,6 +93,7 @@ def diagnosticos(request, paciente_id):
     else:
         return JsonResponse({'error': 'No permitido'})
 
+
 def intervenciones(request, paciente_id):
     if request.method == 'GET':
         intervenciones = Intervencion.objects.filter(paciente=paciente_id)
@@ -75,3 +105,36 @@ def intervenciones(request, paciente_id):
         return JsonResponse(inter_json, safe=False)
     else:
         return JsonResponse({'error': 'No permitido'})
+
+
+def medicamentos(request, paciente_id):
+    if request.method == 'GET':
+        medicamentos = Medicamento.objects.filter(paciente=paciente_id)
+
+        if not medicamentos:
+            return JsonResponse({'warning': 'Paciente sin medicamentos'})
+
+        meds_json = to_json(medicamentos, multi=True)
+        return JsonResponse(meds_json, safe=False)
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
+
+def tomas_signos(request, paciente_id):
+    if request.method == 'GET':
+        tomas = Toma.objects.filter(paciente=paciente_id)
+
+        if not tomas:
+            return JsonResponse({'warning': 'Paciente sin tomas de signos vitales'})
+        
+	tomas_json = to_json(tomas, True)
+
+        for toma_json in tomas_json:
+            signos = SignoVital.objects.filter(toma=toma_json['id'])
+	    signos_json = to_json(signos, True)
+	    toma_json['signos'] = signos_json
+
+        return JsonResponse(tomas_json, safe=False)
+    else:
+        return JsonResponse({'error': 'No permitido'})
+
